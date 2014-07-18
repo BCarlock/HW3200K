c945d012daeab659f8992c49a092d3fe1239218b
BCarlock <bjcarl9138@ung.edu>
2014-07-17 22:55:38 -0400

    Fixed # 2

--- HW5/B_Carlock_Ch6.py
+++ HW5/B_Carlock_Ch6.py
@@ -13,7 +13,9 @@ for field in fieldlist:
     
 import arcpy
 from arcpy import env
-env.workspace = "C:/Data"
+env.overwriteOutput = True
+env.workspace = "C:/EsriPress/Python/Data/Exercise06"
+arcpy.CreateFileGDB_management("C:/EsriPress/Python/Data/Exercise06/Results", "HW5.gdb")
 polylist = arcpy.ListRasters("", "polygon")
 for poly in polylist:
     arcpy.BuildPyramids_managment(polygon)
