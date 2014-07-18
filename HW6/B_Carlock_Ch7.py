e4f5a5cc1d5d92aee718bb7575ac8e4cee932aed
BCarlock <bjcarl9138@ung.edu>
2014-07-18 00:08:55 -0400

    Update B_Carlock_Ch7.py

---
+++ HW6/B_Carlock_Ch7.py
@@ -0,0 +1,39 @@
+# Chapter 7 Question 1
+import arcpy
+from arcpy import env
+env.workspace = "C:/EsriPress/Python/Data/Exercise07"
+fc = "airports.shp"
+seaplane = 'Seaplane Base'
+cursor = arcpy.da.SearchCursor(fc, ["NAME"], '"FEATURE" =\'Seaplane Base\'')
+for row in cursor:
+    arcpy.Buffer_analysis("Seaplane Base", "Seaplane Buffer", "7500 METERS", )
+    
+
+
+import arcpy
+from arcpy import env
+env.workspace = "C:/EsriPress/Python/Data/Exercise07"
+fc = "airports.shp"
+airport = 'airports'
+cursor = arcpy.da.SearchCursor(fc, ["NAME"], '"FEATURE" =\'airports\'')
+for row in cursor:
+    arcpy.Buffer_analysis("airports", "Airport Buffer", "15000 METERS", )
+    
+    
+#or
+import arcpy
+from arcpy import env
+env.workspace = "C:/EsriPress/Python/Data/Exercise07"
+fc = "airports.shp"
+airport = 'airports'
+seaplane = 'Seaplane Base'
+cursor = arcpy.da.SearchCursor(fc, ["NAME"], '"FEATURE" =\'airports\'')
+for row in cursor:
+    arcpy.Buffer_analysis("airports", "Airport Buffer", "15000 METERS", )
+cursor = arcpy.da.SearchCursor(fc, ["NAME"], '"FEATURE" =\'Seaplane Base\'')
+for row in cursor:
+    arcpy.Buffer_analysis("Seaplane Base", "Seaplane Buffer", "7500 METERS", )
+
+
+
+# Chapter 7 Question 2
