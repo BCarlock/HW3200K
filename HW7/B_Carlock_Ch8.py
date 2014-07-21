53aec5c75744ff9cf88a429b045a04466850a327
BCarlock <bjcarl9138@ung.edu>
2014-07-21 16:35:56 -0400

    Updated Ch 8 Challenge 1
    first stab at it

--- HW7/B_Carlock_Ch8.py
+++ HW7/B_Carlock_Ch8.py
@@ -1,5 +1,23 @@
 #Challenge 1
-
+import arcpy
+import fileinput
+import string
+import os
+from arcpy import env
+env.workspace = "P:/Python Class/Python/Data/Exercise08"
+env.overwriteOutput = True
+outpath = "P:/Python Class/Python/Data/Exercise08"
+newfc = "Results/square.shp"
+arcpy.CreateFeatureclass_management(outpath, newfc, "Polygon")
+infile = "P:/Python Class/Python/Data/Exercise08/Results/square.txt"
+cursor = arcpy.da.InsertCursor(newfc, ["SHAPE@"])
+array = arcpy.Array()
+for line in fileinput.input(infile):
+    ID, X, Y = string.split(line, " ")
+    array.add(arcpy.Point(X, Y))
+cursor.insertRow([arcpy.Polygon(array)])
+fileinput.close()
+del cursor
 
 
 
