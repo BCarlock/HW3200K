4bd7eda0a83e9062c41d68c1c8f8b3cbdc9b1922
BCarlock <bjcarl9138@ung.edu>
2014-07-21 16:59:40 -0400

    Updated Ch 8 Challenge 2

--- HW7/B_Carlock_Ch8.py
+++ HW7/B_Carlock_Ch8.py
@@ -22,7 +22,22 @@ del cursor
 
 
 # Challenge 2
+import arcpy
+from arcpy import env
+env.workspace = "P:/Python Class/Python/Data/Exercise08"
+fc = "Hawaii.shp"
+cursor = arcpy.da.SearchCursor(fc, ["SHAPE@"])
+length = 0
 
+for row in cursor:
+    partnum = 0
+    length = 0
+    for part in row[0]:
+        poly = arcpy.Polygon(part)
+        print("Part {0} area: {1}".format(partnum, poly.area))
+        print("Part {0} perimeter: {1}".format(partnum, poly.length))
+
+        partnum += 1
 
 
 
